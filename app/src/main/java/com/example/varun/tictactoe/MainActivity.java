371d72482bd466f16821c015d5c7c0c7b17d7c4a
Varun Rai <raiv95@csu.fullerton.edu>
2018-01-03 16:21:50 -0800

    Change X and O png

--- app/src/main/java/com/example/varun/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/varun/tictactoe/MainActivity.java
@@ -1,53 +1,65 @@
 package com.example.varun.tictactoe;
 
 import android.app.AlertDialog;
-import android.app.usage.UsageEvents;
 import android.content.DialogInterface;
-import android.graphics.drawable.Drawable;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.GridLayout;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.EventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     int player = 0; // player 1 is yellow and player 2 is red
-
+    int player1Wins;
+    int player2Wins;
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    TextView score1;
+    TextView score2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null) {
+            player1Wins = savedInstanceState.getInt("Player1");
+            player2Wins = savedInstanceState.getInt("Player2");
+        }
+
+        score1 = findViewById(R.id.player1Score);
+        score2 = findViewById(R.id.player2Score);
+        score1.setText(String.valueOf(player1Wins));
+        score2.setText(String.valueOf(player2Wins));
+
     }
 
     public void dropIn(View view) {
         ImageView imageView = (ImageView) view;
-        int index = Integer.valueOf(view.getTag().toString());
+        int index = Integer.valueOf(imageView.getTag().toString());
         if (gameState[index] == 2) {
             gameState[index] = player;
             imageView.setTranslationY(-1000f);
             if (player == 0) {
-                imageView.setImageResource(R.drawable.yellow);
+                imageView.setImageResource(R.drawable.opink);
+                Toast.makeText(this, "Pink o", Toast.LENGTH_SHORT).show();
                 player = 1;
             }else if (player == 1) {
-                imageView.setImageResource(R.drawable.red);
+                imageView.setImageResource(R.drawable.xpink);
+                Toast.makeText(this, "Pink x", Toast.LENGTH_SHORT).show();
                 player = 0;
             }
             imageView.animate().translationYBy(1000f);
             int result = checkGameState(gameState);
             if (result == 0) {
+                player1Wins++;
                 playAgain();
             } else if (result == 1) {
+                player2Wins++;
                 playAgain();
             } else { // check to see if there was a scratch
                 int count = 0;
@@ -96,7 +108,11 @@ public class MainActivity extends AppCompatActivity {
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                setContentView(R.layout.activity_main);
+                MainActivity.this.recreate();
+                String temp = String.valueOf(player1Wins);
+                String temp2 = String.valueOf(player2Wins);
+                score1.setText(temp);
+                score2.setText(temp2);
             }
         });
         AlertDialog dialog = builder.create();
@@ -104,4 +120,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("Player1", player1Wins);
+        outState.putInt("Player2", player2Wins);
+        super.onSaveInstanceState(outState);
+    }
 }
